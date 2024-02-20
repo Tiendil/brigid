58c46e24dfb7915f972be2b59cf8fc52de246727
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2024-02-20 18:26:34 +0100

    code formatting

--- brigid/api/http_handlers.py
+++ brigid/api/http_handlers.py
@@ -1,6 +1,8 @@
 import pathlib
 
 import fastapi
+from fastapi.responses import FileResponse, HTMLResponse, PlainTextResponse, RedirectResponse
+
 from brigid.api import renderers
 from brigid.api.sitemaps import build_sitemap_xml
 from brigid.api.static_cache import cache
@@ -8,8 +10,6 @@ from brigid.api.utils import choose_language
 from brigid.core import logging
 from brigid.domain.urls import UrlsRoot
 from brigid.library.storage import storage
-from fastapi.responses import FileResponse, HTMLResponse, PlainTextResponse, RedirectResponse
-
 
 router = fastapi.APIRouter()
 
