08fd1122cbd1c031a414a76f8495646983638500
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2024-08-04 10:52:33 +0200

    formatting

--- brigid/validation/validators.py
+++ brigid/validation/validators.py
@@ -1,9 +1,9 @@
+from brigid.domain import request_context
 from brigid.library.storage import storage
 from brigid.validation.entities import Error
 from brigid.validation.global_validators import required_article
 from brigid.validation.lists_formatting import page_has_correct_list_formatting
 from brigid.validation.page_validators import page_is_rendered
-from brigid.domain import request_context
 
 page_validators = [page_is_rendered, page_has_correct_list_formatting]
 
