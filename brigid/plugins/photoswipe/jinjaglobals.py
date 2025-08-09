fb90621c0c71d791725dc36dd27f195597dcb530
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2025-08-09 16:02:19 +0200

    wip

--- brigid/plugins/photoswipe/jinjaglobals.py
+++
@@ -1,8 +0,0 @@
-from brigid.theme.utils import jinjafilter, jinjaglobal
-from brigid.plugins.photoswipe.settings import settings, Settings
-
-
-# TODO: do we need this now?
-@jinjaglobal
-def photoswipe_settings() -> Settings:
-    return settings
--- brigid/plugins/photoswipe/plugin.py
+++ brigid/plugins/photoswipe/plugin.py
@@ -3,7 +3,6 @@ from typing import Any
 from brigid.plugins.plugin import Plugin
 
 from brigid.theme.templates import get_jinjaglobals
-from brigid.plugins.photoswipe import jinjaglobals
 from brigid.plugins.photoswipe.settings import settings
 
 
@@ -14,9 +13,6 @@ class CorePlugin(Plugin):
 
     def templates_loader(self) -> jinja2.BaseLoader:
         return jinja2.FileSystemLoader(settings.templates, followlinks=True)
-
-    def jinjaglobals(self) -> tuple[dict[str, Any], dict[str, Any]]:
-        return get_jinjaglobals(jinjaglobals)
 
 
 plugin = CorePlugin(slug="photoswipe", settings=settings)
