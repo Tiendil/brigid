1d07eb07623e32e55fc4d2f3875f660c91dfb75e
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2025-08-10 12:14:25 +0200

    wip

--- brigid/jinja2_render/settings.py
+++ brigid/jinja2_render/settings.py
@@ -6,7 +6,7 @@ from brigid.core.settings import BaseSettings
 class Settings(BaseSettings):
     templates_reload: bool = False
 
-    model_config = pydantic_settings.SettingsConfigDict(env_prefix="BRIGID_THEME_")
+    model_config = pydantic_settings.SettingsConfigDict(env_prefix="BRIGID_JINJA2_RENDER_")
 
 
 settings = Settings()
