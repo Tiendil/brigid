68caa66b94a04f01a349476ff2dcc68ec7488e16
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2025-11-08 22:51:53 +0100

    wip

--- brigid/mcp/server.py
+++ brigid/mcp/server.py
@@ -3,8 +3,28 @@ import fastapi
 import fastmcp
 
 from brigid.core import utils
-from brigid.mcp.settings import settings
+from brigid.library.storage import storage
 from brigid.mcp.tools import create_tools
+
+
+def construct_instructions() -> str:
+    site = storage.get_site()
+    site_i18n = site.languages[site.default_language]
+
+    source = [
+        f"This is a personal assistant for readers of the blog {site.url}.",
+        f"The blog title '{site_i18n.title}'.",
+        f"The blog description is: {site_i18n.subtitle}",
+        f"The blog author is {site_i18n.author}.",
+        f"The license of the blog content is {site_i18n.license}." if site_i18n.license else None,
+        f"The blog is multi-lingual and supports the following languages: {', '.join(site.allowed_languages)}." if len(site.allowed_languages) > 1 else None,
+        f"The default language of the blog is {site.default_language}." if len(site.allowed_languages) > 1 else None,
+        f"The content of the blog is stored in the repository: {site.content_repository}." if site.content_repository else None,
+    ]
+
+    source = [instruction for instruction in source if instruction]
+
+    return '\n'.join(source)
 
 
 # We create MCP instance dymanically because:
@@ -12,15 +32,14 @@ from brigid.mcp.tools import create_tools
 # - we may need to construct multiple MCPs (per language) in the future
 def create_mcp(app: fastapi.FastAPI) -> fastapi.FastAPI:
 
-    instructions = """
-    Do smth
-    """
+    site = storage.get_site()
+    site_i18n = site.languages[site.default_language]
 
     # TODO: add maximum configuration
     # TODO: we may want to create an MCP per language
     # TODO: website_url, icons (fastmcp 2.14.0+)
-    mcp = fastmcp.FastMCP(name=settings.name,
-                          instructions=instructions,
+    mcp = fastmcp.FastMCP(name=site_i18n.title,
+                          instructions=construct_instructions(),
                           version=utils.version(),
                           auth=None,
                           strict_input_validation=False,  # allow Pydantic to be flexible
