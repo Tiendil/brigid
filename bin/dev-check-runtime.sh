d8081672295f5cc4e1f8a80b6221147fdb259988
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2026-03-03 11:56:10 +0100

    wip

--- bin/dev-check-runtime.sh
+++ bin/dev-check-runtime.sh
@@ -2,16 +2,8 @@
 
 set -e
 
-CONTAINER="brigid:check-runnable-in-prod"
-
-echo "build container"
-
-docker build -t $CONTAINER -f ./docker/Dockerfile .
-
-echo "run checks"
-
 echo "check that CLI works"
-docker run --rm $CONTAINER brigid --help
+./bin/utils.sh poetry run brigid --help
 
 echo "check that CLI shows configs"
-docker run --rm $CONTAINER brigid print-configs
+./bin/utils.sh poetry run brigid print-configs
