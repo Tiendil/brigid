681595931afb9cec4b78eb4719b86c30d73fb8e3
Aliaksei Yaletski <a.eletsky@gmail.com>
2026-03-03 13:22:17 +0100

    Removed too restrictive error on rendering external links. Now, external links can target the same domain as the current page (#130)

--- brigid/markdown_render/processors/external_links.py
+++ brigid/markdown_render/processors/external_links.py
@@ -3,7 +3,6 @@ from urllib.parse import urlparse
 from markdown.inlinepatterns import LINK_RE as EXTERNAL_LINK_RE
 from markdown.inlinepatterns import LinkInlineProcessor
 
-from brigid.library.storage import storage
 from brigid.markdown_render.context import render_context
 
 
@@ -23,21 +22,9 @@ class ExternalLinkInlineProcessor(LinkInlineProcessor):
 
         parsed_url = urlparse(href)
 
-        site = storage.get_site()
-
         context = render_context.get()
 
-        parsed_site = urlparse(site.url)
-
         # validate external links
-
-        # TODO: compare taking into account the default ports, aka example.com:90 = example.com
-        # TODO: is this required, link could be to the same domain but in different project
-        if parsed_site.netloc == parsed_url.netloc:
-            context.add_error(
-                failed_text=data, message="Use internal links syntax to specify links to the same domain"
-            )
-            return result
 
         if parsed_url.scheme == "":
             context.add_error(failed_text=data, message="Specify schema/protocol for external links")
---
+++ changes/next_release.md
@@ -0,0 +1,2 @@
+
+- Removed too restrictive error on rendering external links. Now, external links can target the same domain as the current page.
